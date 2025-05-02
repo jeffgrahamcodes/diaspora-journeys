c7c3072f5a7323b71759974d4564376f1f248a29
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-02 16:17:46 -0400

    chore: add middleware tp check req.body

--- routes/journeyRoutes.js
+++ routes/journeyRoutes.js
@@ -7,13 +7,14 @@ const {
   updateJourney,
   deleteJourney,
   checkId,
+  checkBody,
 } = require('../controllers/journeyControllers');
 
 const router = express.Router();
 
 router.param('id', checkId);
 
-router.route('/').get(getAllJourneys).post(createJourney);
+router.route('/').get(getAllJourneys).post(checkBody, createJourney);
 
 router.route('/:id').get(getJourney).patch(updateJourney).delete(deleteJourney);
 
