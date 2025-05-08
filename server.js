871ef21c6235296754a0ce3a544a7e0a733e33cf
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-07 21:43:53 -0400

    chore: install mongoose

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
   path: './.env'
 });
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'));
 
 const app = require('./app');
 
